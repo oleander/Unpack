88184d32c2722b940af644f77c1c33d010e3e32b
Linus Oleander <linus@oleander.nu>
2011-02-05 06:29:35 +0100

    Using the tmp dir as unpack zone

--- spec/unpack_spec.rb
+++ spec/unpack_spec.rb
@@ -330,8 +330,9 @@ describe Unpack, "should be able to unpack" do
   end
   
   it "should work with folders that contain whitespace" do
+    %x{mkdir -p '/tmp/I Spit on Your Grave[2010][Unrated Edition]DvDrip[Eng]-FXG'}
     lambda {
-      Unpack.it!(:file => "spec/data/from/test_package.rar", :to => 'spec/data/folders/I Spit on Your Grave[2010][Unrated Edition]DvDrip[Eng]-FXG')
+      Unpack.it!(:file => "spec/data/from/test_package.rar", :to => '/tmp/I Spit on Your Grave[2010][Unrated Edition]DvDrip[Eng]-FXG')
     }.should_not raise_error(Exception)
   end
 end
